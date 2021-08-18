e3e08169f3a19fcf271bccf35257f29fb980110c
NguyenTien110 <tienmetien011111@gmail.com>
2021-08-18 21:58:10 +0700

    add structure streaming example and init warning job thread

--- src/main/java/yourway/WarningJob.java
+++ src/main/java/yourway/WarningJob.java
@@ -1,26 +1,35 @@
 package yourway;
 
-import org.apache.spark.sql.Column;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.api.java.UDF1;
-import org.apache.spark.sql.expressions.UserDefinedFunction;
 import org.apache.spark.sql.streaming.StreamingQuery;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.functions.*;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import scala.collection.Seq;
 import settings.Settings;
+import scala.collection.JavaConverters;
 
-import static org.apache.spark.sql.functions.*;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.json_tuple;
+import static org.apache.spark.sql.types.DataTypes.StringType;
 
 public class WarningJob {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException, StreamingQueryException {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Warning Job Yourway")
                 .getOrCreate();
-
+        spark.sparkContext().setLogLevel("ERROR");
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
@@ -28,14 +37,27 @@ public class WarningJob {
                 .option("subscribe", Settings.TOPIC_JOB)
                 .load();
 
-        UserDefinedFunction decode_string = udf((UDF1<Byte, Object>) Object::toString, DataTypes.StringType);
+        Seq<String> seq = JavaConverters.asScalaIteratorConverter(Settings.FIELD_JOB.iterator()).asScala().toSeq();
 
-//        Dataset<Row> data = df.select(
-//                    json_tuple(
-//                            udf((Byte x) -> x, DataTypes.StringType),
-//                            "id"
-//            ).alias("id")
-//        );
+
+        Dataset<Row> data = df.select(
+                json_tuple(
+                        col("value").cast(StringType), seq
+                )
+        );
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Settings.KAFKA_URI);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "user");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(Settings.TOPIC_USER_QUERY));
+        ConsumerRecords<String, String> records = consumer.poll(100);
+        System.out.println(records);
+        for (ConsumerRecord<String, String> record : records)
+            System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
 
 //        check_matching = udf(
 //                lambda address, age, salary, year, edu_level, job_attribute: matching(address, age, salary, year, edu_level,
@@ -52,14 +74,19 @@ public class WarningJob {
 //
 //        data = data.filter(col("value") != "")
 
-//        StreamingQuery ds = df
+        StreamingQuery ds = data
+                .writeStream()
+                .format("console")
+                .start();
+
+//        StreamingQuery ds = data
 //                .writeStream()
 //                .format("kafka")
 //                .option("kafka.bootstrap.servers", Settings.KAFKA_URI)
+//                .option("checkpointLocation", Settings.CHECKPOINT_PATH)
 //                .option("topic", Settings.TOPIC_USER)
 //                .start();
-//
-//        ds.awaitTermination();
-    }
 
+        ds.awaitTermination();
+    }
 }
