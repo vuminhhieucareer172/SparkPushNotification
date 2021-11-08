e8f35c552fd6e83d5b7d086fa9f3c14b0072ced1
edwardngo <hoangso8000@gmail.com>
2021-11-08 22:15:42 +0700

    .

--- frontend/src/app/pages/query/manage-queries/manage-queries.component.ts
+++ frontend/src/app/pages/query/manage-queries/manage-queries.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { SmartTableData } from '../../../@core/data/smart-table';
 
 @Component({
   selector: 'ngx-manage-queries',
   templateUrl: './manage-queries.component.html',
   styleUrls: ['./manage-queries.component.scss'],
 })
-export class ManageQueriesComponent implements OnInit {
+export class ManageQueriesComponent {
 
-  constructor() { }
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      name: {
+        title: 'Name',
+        type: 'string',
+      },
+      position: {
+        title: 'Position',
+        type: 'string',
+      },
+      office: {
+        title: 'Office',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+      salary: {
+        title: 'Salary',
+        type: 'number',
+      },
+      actions: {
+        title: 'Actions',
+        type: 'number',
+      },
+    },
+  };
 
-  ngOnInit(): void {
+  // Viet API o day
+  // source: LocalDataSource = new LocalDataSource();
+
+  // constructor(private service: SmartTableData) {
+  //   const data = this.service.getData();
+  //   this.source.load(data);
+  // }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
   }
-
 }
