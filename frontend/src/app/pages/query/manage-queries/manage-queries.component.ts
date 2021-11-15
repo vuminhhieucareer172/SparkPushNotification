e0287003d89bf581632282416c0e649057bc4fe9
vuminhhieucareer172 <84167313+vuminhhieucareer172@users.noreply.github.com>
2021-11-15 15:44:08 +0700

    manage query delete API, get query API

--- frontend/src/app/pages/query/manage-queries/manage-queries.component.ts
+++ frontend/src/app/pages/query/manage-queries/manage-queries.component.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NbGlobalPhysicalPosition, NbToastrService } from '@nebular/theme';
 import { LocalDataSource } from 'ng2-smart-table';
 import { SmartTableData } from '../../../@core/data/smart-table';
+import { SERVER_API_URL } from '../../../app.constants';
 
 @Component({
   selector: 'ngx-manage-queries',
@@ -9,62 +14,127 @@ import { SmartTableData } from '../../../@core/data/smart-table';
 })
 export class ManageQueriesComponent {
 
+  source: LocalDataSource = new LocalDataSource();
+  data: Array<any>;
+  destroyByClick = true;
+  duration = 2000;
+  hasIcon = true;
+  preventDuplicates = false;
+
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder,
+    private toastrService: NbToastrService,
+    private router: Router) {
+    this.http.get(SERVER_API_URL + '/query', { observe: 'response' })
+      .subscribe(
+        res => {
+          this.data = Object.keys(res.body).map((key) => res.body[key]);
+          this.source.load(this.data);
+        }, (error) => {
+          this.showToast('An unexpected error occured', error.message, 'danger');
+          this.data = [];
+        }, () => { },
+      );
+  }
+
+  private showToast(title: string, body: string, typeStatus: string) {
+    const config = {
+      status: typeStatus,
+      destroyByClick: this.destroyByClick,
+      duration: this.duration,
+      hasIcon: this.hasIcon,
+      position: NbGlobalPhysicalPosition.TOP_RIGHT,
+      preventDuplicates: this.preventDuplicates,
+    };
+    const titleContent = title ? title : 'error';
+
+    this.toastrService.show(
+      body,
+      titleContent,
+      config);
+  }
+
   settings = {
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-    delete: {
-      deleteButtonContent: '<i class="nb-trash"></i>',
-      confirmDelete: true,
+    actions: {
+      edit: false,
+      add: false,
+      delete: false,
+      position: 'right',
+      width: '40px',
+      custom: [
+        {
+          name: 'edit-query',
+          width: '20px',
+          title: '<i class="nb-edit" title="Edit query"></i>',
+        },
+        {
+          name: 'delete',
+          width: '20px',
+          title: '<i class="nb-trash" title="Delete query"></i>',
+        },
+      ],
     },
     columns: {
-      name: {
-        title: 'Name',
+      index: {
+        title: 'Index',
+        type: 'number',
+        width: '20px',
+        valuePrepareFunction: (value, row, cell) => {
+          const pager = this.source.getPaging();
+          return (pager.page - 1) * pager.perPage + cell.row.index + 1;
+         },
+      },
+      sql: {
+        title: 'Sql',
         type: 'string',
       },
-      position: {
-        title: 'Position',
+      topic_kafka_output: {
+        title: 'Topic_kafka_output',
         type: 'string',
       },
-      office: {
-        title: 'Office',
+      time_trigger: {
+        title: 'Time_trigger',
         type: 'string',
       },
-      age: {
-        title: 'Age',
-        type: 'number',
+      created_at: {
+        title: 'Created_at',
+        type: 'string',
       },
-      salary: {
-        title: 'Salary',
-        type: 'number',
+      updated_at: {
+        title: 'Updated_at',
+        type: 'string',
       },
-      actions: {
-        title: 'Actions',
-        type: 'number',
+      contact: {
+        title: 'Contact',
+        type: 'string',
+        valuePrepareFunction: (contact) => JSON.stringify(contact),
       },
     },
   };
 
-  // Viet API o day
-  // source: LocalDataSource = new LocalDataSource();
-
-  // constructor(private service: SmartTableData) {
-  //   const data = this.service.getData();
-  //   this.source.load(data);
-  // }
-
-  onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
+  onCustom(event) {
+    if (event.action === 'edit-query') {
+      this.router.navigate(['/query/' + event.data.id]);
+    } else if (event.action === 'delete') {
+      const res = confirm(`Are you sure want to delete this config`);
+      if (res) {
+        this.http.delete(SERVER_API_URL + '/query/' + event.data.id, {observe: 'response'})
+        .subscribe(
+          res => {
+            this.showToast('Successfull', '', 'success');
+            this.http.get(SERVER_API_URL + '/query', {observe: 'response'})
+              .subscribe(
+                res => {
+                  this.data = Object.keys(res.body).map((key) => res.body[key]);
+                  this.source.load(this.data);
+                },
+              );
+          }, (error) => {
+            this.showToast('An unexpected error occured', error.error.message, 'danger');
+          }, () => {},
+        );
+      }
     }
   }
 }
