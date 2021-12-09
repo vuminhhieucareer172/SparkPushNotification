1131705164d5aa7e4b15a184a6af254467dc8d29
NguyenTien110 <tienmetien011111@gmail.com>
2021-12-09 21:41:10 +0700

    kill job streaming when call api stop

--- backend/controller/stream.py
+++ backend/controller/stream.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import signal
 
 from apscheduler.triggers.cron import CronTrigger
 from fastapi import status
@@ -168,6 +170,7 @@ def delete_stream(stream_name: str, db: DB):
 def stop_job_streaming():
     try:
         Spark().get_sql_context().sparkSession.stop()
+        os.kill(Spark().get_pid(), signal.SIGSTOP)
     except Exception as e:
         print(e)
         return JSONResponse(content={"message": "Error: {}".format(str(e))}, status_code=status.HTTP_400_BAD_REQUEST)
